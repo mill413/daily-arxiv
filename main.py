7fc991f9b07343683a3315efb1a218187e6e6365
mill413 <mill413@outlook.com>
2024-01-15 20:40:01 +0800

    rename keywords to topics

--- main.py
+++ main.py
@@ -8,19 +8,17 @@ max_results = config["max_results"]
 client = arxiv.Client()
 
 content: dict[str, list[Paper]] = {}
-for k in config["keywords"]:
-    topic: str = k["topic"]
+for k in config["topics"]:
+    topic: str = k["name"]
 
     log(f"Query topic {topic}")
     content[topic] = parse_papers(client.results(arxiv.Search(
         query=concat_filters(k["filters"]),
         max_results=max_results,
         sort_by=arxiv.SortCriterion.SubmittedDate
-    )))
+    ))).sort(reverse=True)
 
-    assert len(content[topic]) > 0, f"content{topic} empty"
-
-    content[topic].sort(reverse=True)
+    assert len(content[topic]) > 0, f"Content of {topic} empty"
 
     log(f"Get code link of {topic}")
     for paper in content[topic]:
